47cde9e13381143ff3e0327289c3658f6671f1e4
Smriti Gupta <76395337+contactsmriti@users.noreply.github.com>
2022-05-28 16:44:57 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,97 @@
+import cv2
+import os
+import face_recognition
+import numpy as np
+from datetime import datetime
+
+cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
+haar_model = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
+face_cascade=cv2.CascadeClassifier(haar_model)
+
+
+path = 'images'
+images = []
+personNames = []
+myList = os.listdir(path)
+print(myList)
+for cu_img in myList:
+    current_Img = cv2.imread(f'{path}/{cu_img}')
+    images.append(current_Img)
+    personNames.append(os.path.splitext(cu_img)[0])
+print(personNames)
+
+def attendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            time_now = datetime.now()
+            tStr = time_now.strftime('%H:%M:%S')
+            dStr = time_now.strftime('%d/%m/%Y')
+            f.writelines(f'\n{name},{tStr},{dStr}')
+
+
+def faceEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = faceEncodings(images)
+print('All Encodings Complete!!!')
+
+
+
+ds_factor=0.6
+class VideoCamera(object):
+    def __init__(self):
+       #capturing video
+       self.video = cv2.VideoCapture(0)
+    
+    def __del__(self):
+        #releasing camera
+        self.video.release()
+
+    def get_frame(self):
+       #extracting frames
+        ret, frame = self.video.read()
+
+        # frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,
+        # interpolation=cv2.INTER_AREA)                    
+        # gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        # face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+        # for (x,y,w,h) in face_rects:
+        #     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        #     break        
+        
+        faces = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+        faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+
+        facesCurrentFrame = face_recognition.face_locations(faces)
+        encodesCurrentFrame = face_recognition.face_encodings(faces, facesCurrentFrame)
+
+        for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            # print(faceDis)
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = personNames[matchIndex].upper()
+                print(name)
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                attendance(name)
+        
+        
+        # encode OpenCV raw frame to jpg and displaying it
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
