18fc64044515ec82c834681e4ca85cb38c0be69f
Smriti Gupta <76395337+contactsmriti@users.noreply.github.com>
2022-05-29 11:59:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,5 @@ def video_feed():
     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
